8a49b8aab6e347f17c03aee268cd1617c6b3fdc7
Dennis Wollersheim <dewoller@gmail.com>
2023-02-09 11:51:41 +0000

    got a few details, price and sale date.  Renamed json fields to _json

--- _targets.R
+++ _targets.R
@@ -11,32 +11,31 @@ base_url= "https://www.realestate.com.au/sold"
 extract_pattern= "/[^/]*$"
 
 
-https://www.realestate.com.au/sold/property-villa-qld-palmwoods-138614379
+# https://www.realestate.com.au/sold/property-villa-qld-palmwoods-138614379
 
-property_url_df %>%
-	head(10) %>%
-	pull( property_url )
+# property_url_df %>%
+#     head(10) %>%
+#     pull( property_url )
 
 tar_plan(
 
 
-df=dir_ls('property_page_cache/archive/', glob='*.html') %>%
-d	tibble( property_page = .) %>%
-	mutate( row = row_number()) %>%
-	# head(24) %>%
-	# tail(1) %>%
-	# pluck( 'property_page') %>%
-	# { . } -> property_page
-	mutate( details = map2( property_page, row, extract_details_from_page )) %>%
-	unnest( details ) %>%
-	mutate( url = property_page %>%
-		str_extract( extract_pattern) %>%
-		str_remove( '.html$') %>%
-		str_c( base_url, .) ) %>%
-	select( url, everything() )
+df=
+
+dir_ls("property_page_cache/archive/", glob = "*.html") %>%
+    tibble(property_page = .) %>%
+    mutate(row = row_number()) %>%
+    # pluck("property_page") %>%
+    mutate(details = map(property_page,  extract_details_from_page)) %>%
+    unnest(details) %>%
+    mutate(url = property_page %>%
+        str_extract(extract_pattern) %>%
+        str_remove(".html$") %>%
+        str_c(base_url, .)) %>%
+    select(url, everything())
 
 ,
 
-write = df %>% write_csv( 'output/property_details.csv')
+write = df %>% write_csv( 'output/property_details3.csv')
 
 )
